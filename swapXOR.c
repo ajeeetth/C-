0f4acbc28d981d1432de3f263733b728c75fb669
Ajith S Devadiga <43115384+ajeeetth@users.noreply.github.com>
2020-06-22 22:17:40 +0530

    Update swapXOR.c

--- swapXOR.c
+++ swapXOR.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
+void swaping(int, int);
 int main(){
     int a = 10, b = 50;
     printf("Before Swapping a = %d b = %d\n", a, b);
-
-    a = a ^ b;
+	swaping(a, b);
+	return 0;
+}
+void swaping(int a,  int b){
+	a = a ^ b;
     b = a ^ b;
     a = a ^ b;
-
     printf("After Swapping a = %d b = %d\n", a, b);
-
-    return 0;
 }
+	
