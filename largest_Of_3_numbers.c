7b88822496bbbdd5b7b7b6d79e994c991d676b65
Ajith S Devadiga <43115384+ajeeetth@users.noreply.github.com>
2020-06-22 22:49:05 +0530

    Update largest_Of_3_numbers.c

--- largest_Of_3_numbers.c
+++ largest_Of_3_numbers.c
@@ -1,20 +1,9 @@
-#include <stdio.h> 
-
-int main() { 
-	int first, second, third; 
-
-	printf("Enter the 3 numbers : "); 
-	scanf("%d %d %d", &first, &second, &third); 
-
-	if (first >= second && first >= third) 
-		printf("%d is the largest number.", first); 
-
-	if (second >= first && second >= third) 
-		printf("%d is the largest number.", second); 
-
-	if (third >= first && third >= second) 
-		printf("%d is the largest number.", third); 
-
-	return 0; 
-} 
-
+#include<stdio.h>
+int main(){
+    int first, second, third;
+    printf("Enter 3 numbers\n");
+    scanf("%d %d %d", &first, &second, &third);
+    int big = first>second ? (first>third ? first:third) : (second>third ? second:third);
+    printf("biggest of three numbers is %d\n", big);
+    return 0;
+}
